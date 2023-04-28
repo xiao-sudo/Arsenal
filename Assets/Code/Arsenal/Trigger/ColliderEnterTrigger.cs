99ba0b20c52b2f1be9c27388ea449cf4d86a6be0
xiao-sudo <85089925@qq.com>
2023-04-28 17:36:25 +0800

    1. add ColliderEnterTrigger

---
+++ Assets/Code/Arsenal/Trigger/ColliderEnterTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Arsenal.Trigger
+{
+    public class ColliderEnterTrigger : BehaviourTrigger
+    {
+        [SerializeField]
+        private Collider m_Collider;
+
+        public Func<Collider, bool> Condition { get; set; }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (null == Condition || Condition.Invoke(other))
+                DoFire();
+        }
+
+        protected virtual void DoFire()
+        {
+            Fire(null);
+        }
+    }
+}

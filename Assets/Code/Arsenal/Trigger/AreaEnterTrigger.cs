d586527f21f52307a485476275d85b46dd47092a
xiao-sudo <85089925@qq.com>
2023-05-05 17:33:05 +0800

    1. add AreaEnterTrigger 2. remove ColliderEnterTrigger, use AreaEnterTrigger and BehaviourArea

---
+++ Assets/Code/Arsenal/Trigger/AreaEnterTrigger.cs
@@ -0,0 +1,24 @@
+﻿using Code.Arsenal.Area;
+using UnityEngine;
+
+namespace Code.Arsenal.Trigger
+{
+    public class AreaEnterTrigger : BehaviourTrigger
+    {
+        [SerializeField]
+        private BehaviourArea m_Area;
+
+        public override void Init()
+        {
+            base.Init();
+
+            if (null != m_Area)
+                m_Area.OnTouristEnter = OnTouristEnter;
+        }
+
+        private void OnTouristEnter(IAreaTourist area_tourist)
+        {
+            Fire(area_tourist);
+        }
+    }
+}

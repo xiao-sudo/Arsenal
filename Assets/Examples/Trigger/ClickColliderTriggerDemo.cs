4eb817639b850ee1bfbb6f14251e7b171a35fddf
xiao-sudo <85089925@qq.com>
2023-05-06 18:43:16 +0800

    1. Add ClickColliderTriggerDemo

---
+++ Assets/Examples/Trigger/ClickColliderTriggerDemo.cs
@@ -0,0 +1,26 @@
+﻿using Code.Arsenal.Trigger;
+using UnityEngine;
+
+namespace Examples.Trigger
+{
+    public class ClickColliderTriggerDemo : MonoBehaviour
+    {
+        [SerializeField]
+        private LayerMask m_Mask = ~0;
+
+        [SerializeField]
+        private float m_Distance = 50f;
+
+        private void Update()
+        {
+            if (DemoInput.LeftMouseUp)
+            {
+                if (Camera.main != null)
+                {
+                    var ray = Camera.main.ScreenPointToRay(DemoInput.MousePosition);
+                    ColliderRayCastTrigger.FireOneRayHitTrigger(ray, m_Mask, m_Distance, null);
+                }
+            }
+        }
+    }
+}

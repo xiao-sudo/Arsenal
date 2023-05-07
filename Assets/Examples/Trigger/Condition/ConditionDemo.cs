8009e67221576c49fa5677c6921719b740f3abb9
xiao-sudo <85089925@qq.com>
2023-05-07 15:46:28 +0800

    1. add ConditionDemo

---
+++ Assets/Examples/Trigger/Condition/ConditionDemo.cs
@@ -0,0 +1,21 @@
+﻿using Code.Arsenal.Trigger;
+using UnityEngine;
+
+namespace Examples.Trigger.Condition
+{
+    public class ConditionDemo : ConditionTrigger
+    {
+        public override void Init()
+        {
+            base.Init();
+
+            FireEvent = o => { Debug.Log("Condition Fire"); };
+        }
+
+        private void Update()
+        {
+            if (DemoInput.LeftMouseUp)
+                TryFire(null);
+        }
+    }
+}
---
+++ Assets/Examples/Trigger/Condition/InAreaCondition.cs
@@ -0,0 +1,28 @@
+﻿using Code.Arsenal.Area;
+using Code.Arsenal.Condition;
+using UnityEngine;
+
+namespace Examples.Trigger.Condition
+{
+    public class InAreaCondition : BehaviourCondition
+    {
+        [SerializeField]
+        private AreaTourist m_Tourist;
+
+        [SerializeField]
+        private BehaviourArea m_Area;
+
+        public override bool Eval()
+        {
+            return base.Eval() && InArea();
+        }
+
+        private bool InArea()
+        {
+            if (null == m_Area || null == m_Tourist)
+                return false;
+
+            return m_Area.IsTouristInArea(m_Tourist);
+        }
+    }
+}
